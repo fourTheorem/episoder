9e394639173ab9336eef51d0d010550c273259b1
Luciano Mammino <lmammino@users.noreply.github.com>
2026-02-03 16:22:06 +0100

    feat: integration with podwhisperer v2

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,6 +11,19 @@ import { logger } from './lambda-common'
  * @returns The retrieved JSON as an object
  */
 export async function getS3JSON<T = unknown>(s3Client: S3Client, bucket: string, key: string): Promise<T> {
+  const text = await getS3Text(s3Client, bucket, key)
+  return JSON.parse(text)
+}
+
+/**
+ * Convenience function for retrieving text content from S3
+ *
+ * @param s3Client An AWS SDK v3 S3 Client
+ * @param bucket The S3 bucket name
+ * @param key The S3 object key
+ * @returns The retrieved content as a string
+ */
+export async function getS3Text(s3Client: S3Client, bucket: string, key: string): Promise<string> {
   logger.info('Getting object', { bucket, key })
   const response = await s3Client.send(
     new GetObjectCommand({
@@ -24,5 +37,5 @@ export async function getS3JSON<T = unknown>(s3Client: S3Client, bucket: string,
     chunks.push(chunk as Uint8Array)
   }
 
-  return JSON.parse(Buffer.concat(chunks).toString('utf-8'))
+  return Buffer.concat(chunks).toString('utf-8')
 }
